5941dc559b86f64c896ba14243904d76998eb0ab
WMCharles <charles.wafula@student.moringaschool.com>
2022-09-21 10:41:45 +0300

    add main component

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Messages from "./Messages";
+import Form from "./Form";
+
+function Main(){
+    return (
+        <main>
+            <h2>Messages</h2>
+            <Messages/>
+            <Form/>
+        </main>
+    )
+}
+
+export default Main;
