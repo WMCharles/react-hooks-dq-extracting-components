1b11962775aeae64bfe38f941b69214d1b1cea5c
WMCharles <charles.wafula@student.moringaschool.com>
2022-09-21 10:42:31 +0300

    add navbar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function NavBar(){
+    return (
+        <nav>
+            <h2>Contacts</h2>
+            <ul className="contacts">
+            <li className="contact">
+                <div className="icon">A</div>
+                Angela
+            </li>
+            <li className="contact active">
+                <div className="icon">P</div>
+                Pamela
+            </li>
+            <li className="contact">
+                <div className="icon">S</div>
+                Sandra
+            </li>
+            <li className="contact">
+                <div className="icon">R</div>
+                Rita
+            </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar;
